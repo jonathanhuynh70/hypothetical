cd8d2396feb37c740e7be566442a42846b0569e2
jon <jon.huynh14@gmail.com>
2026-04-10 19:22:18 +1000

    added voting mechanic, added TODO to flesh things out

--- hypothetical/views.py
+++ hypothetical/views.py
@@ -1,21 +1,35 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from . import models
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Choice, Question, Vote
+from django.urls import reverse
 
 def index(request):
-    questions = models.Question.objects.all()
+    questions = Question.objects.all()
     context = {'latest_question_list': questions} 
     return render(request, 'hypothetical/index.html', context)
 
 def detail(request, question_id):
-    question = get_object_or_404(models.Question, pk=question_id)
-    return render(request, 'hypothetical/detail', {'question': question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'hypothetical/detail.html', {'question': question})
 
 def results(request, question_id):
-    question = get_object_or_404(models.Question, pk=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'hypothetical/results', {'question': question})
 
 def vote(request, question_id):
-    question = get_object_or_404(models.Question, pk=question_id)
-    return render(request, 'hypothetical/vote', {'question': question})
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = Question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist): 
+        return render(request,
+               "hypothetical/detail", 
+               {
+                   "question": question,
+                   "error_message": "You didn't pick a choice sweetie"
+               }
+        )
+    else: 
+        Vote.objects.create(choice=selected_choice, voter=selected_choice.author)
+
+    return HttpResponseRedirect(reverse("hypothetical:results", args=(question.id)))
 
